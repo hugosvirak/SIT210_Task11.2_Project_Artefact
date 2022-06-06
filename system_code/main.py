10f73b346d68057e2201fe506d5cb7d2777c43f0
Hugo L <hugo.luccarini@gmail.com>
2022-06-06 14:11:06 +0200

    remove redundant imports

--- system_code/main.py
+++ system_code/main.py
@@ -1,9 +1,6 @@
 # import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import time
-import cv2
-import pytesseract
 import cv2 
 import argparse 
 import os 
